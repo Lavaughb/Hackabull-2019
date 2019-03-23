4323c0151f853dac83e352b1d091b14a21d61c74
Lavaugh Brown <35616278+Lavaughb@users.noreply.github.com>
2019-03-23 19:39:22 -0400

    inital first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sub from './sub';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+          <div className="nav-bar">
+            <div className="nav-bar-elements">Donatabull</div>
+          </div>
+          <header className="App-header">
+            <div>
+              <header>Question 1</header>
+              <h1 className="questions">Who are you</h1>
+              <h1 className="questions">Question 2</h1>
+              <h1 className="questions">Who are you</h1>
+              <h1 className="questions">Question 2</h1>
+              <h1 className="questions">Who are you</h1>
+            </div>
+          </header>
+        </div>
     );
   }
 }
