91e8186cd2c17e127fb6e2582ec6abd178aa739e
Lavaugh Brown <35616278+Lavaughb@users.noreply.github.com>
2019-03-24 05:47:37 -0400

    functionality

--- src/sub.js
+++ src/sub.js
@@ -2,25 +2,25 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardLink, CardTitle, CardSubtitle } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-const Example = (props) => {
-  return (
-    <div>
-      <Card>
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-        </CardBody>
-        <img width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <CardLink href="#">Card Link</CardLink>
-          <CardLink href="#">Another Link</CardLink>
-        </CardBody>
-      </Card>
-    </div>
-  );
+class Sub extends Component {
+  
+  
+  render(){
+    return (
+      <div>
+        <header className="App-header">
+              <div>
+                <header className="head-tag">Which of these has the most importance?</header>
+                <input type="button" className="questions" value="Input Button"></input>
+                <input type="button" className="questions" value="Input Button"></input>
+                <input type="button" className="questions" value="Input Button"></input>
+                <input type="button" className="questions" value="Input Button"></input>
+              </div>
+            </header>
+      </div>
+    );
+  }
 };
 
 
-export default Example;
+export default Sub;
